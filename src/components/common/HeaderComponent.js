59819f5e497b7a1b63a4fdbf2c4071e22b2c975a
조준희 <iopp3423@gmail.com>
2024-04-18 21:09:27 +0900

    [UPDATE] 메인화면 철자 변경, 회원가입 시 오류문구 수정

--- src/components/common/HeaderComponent.js
+++ src/components/common/HeaderComponent.js
@@ -7,7 +7,7 @@ const Header = () =>{
     return(
         <>
         <div className = "w-full h-16 flex justify-between border border-b border-stone-300">
-            <Link to = {"/"} className = "flex items-center w-full h-full pl-6 text-xl font-semibold">Argorithm Review</Link>
+            <Link to = {"/"} className = "flex items-center w-full h-full pl-6 text-xl font-semibold">Algorithm Review</Link>
             <Link to = {'/mypage'} className = "pr-6 flex items-center h-full">
                 <img className = "w-7 h-6" src = "https://cdn-icons-png.flaticon.com/128/747/747376.png"/>
             </Link>
--- src/components/member/SignupComponent.js
+++ src/components/member/SignupComponent.js
@@ -23,16 +23,24 @@ const SignupComponent = () =>{
     
       const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-          const response = await axios.post(`https://${process.env.REACT_APP_BASE_URL}/user/signup`, formData);
-          if(response.status === 201){
-            navigate('/login');
-          }
-        } catch (error) {
-          console.error('Error:', error);
-          //setError(error);
-          setEmail(true)
-          console.log('요청실패')
+        
+        // 비밀번호가 특수문자가 최소 1개, 알파벳이 포함되어야 하며, 8~20글자 사이여야 함
+        const passwordRegex = /^(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,20}$/;
+        if (!passwordRegex.test(formData.password)) {
+            setError('특수문자가 1개 알파벳 조합 8~20글자로 작성해주세요');
+        }
+        else{
+            try {
+            const response = await axios.post(`https://${process.env.REACT_APP_BASE_URL}/user/signup`, formData);
+            if(response.status === 201){
+             navigate('/login');
+            }
+            } catch (error) {
+            console.error('Error:', error);
+            setError(error);
+            setEmail(true)
+            console.log('요청실패')
+            }
         }
       };
 
@@ -78,7 +86,7 @@ const SignupComponent = () =>{
                                 placeholder="••••••••" 
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
                             </div>
-                            {email ? <div className = "h-1 pb-2 text-sm text-red-400 font-medium">{error}</div> : <></>}
+                            {error ? <div className = "h-1 pb-2 text-sm text-red-400 font-medium">{error}</div> : <></>}
                             <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">계정 생성</button>
                             <p class="text-sm font-light text-gray-500">
                                 이미 계정이 있으신가요? <Link to='/login' class="font-medium text-black hover:underline">로그인</Link>
