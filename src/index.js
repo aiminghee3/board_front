41594df57b9098bdc0b30459c819c7fa71c022d3
조준희 <iopp3423@gmail.com>
2024-07-17 21:04:33 +0900

    refactor: 서비스워커 로직수정

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import {BrowserRouter} from "react-router-dom";
 
+// 서비스 워커 등록 로직 추가
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RecoilRoot>
